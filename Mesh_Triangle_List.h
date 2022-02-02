0349f102888e31ffd905c4406035ee84ba627e59
Abhay Joseph <abhayjsph2002@gmail.com>
2022-02-02 14:40:51 +0530

    added Mesh_Triangle_List class

---
+++ Mesh_Triangle_List.h
@@ -0,0 +1,23 @@
+#include<iostream>
+#include"TriangleClass.h"
+
+using namespace std;
+
+typedef struct node {
+  Triangle t;
+  struct node *next;
+  struct node *prev;
+} Mesh_Triangle_Node_t;
+
+typedef struct _list {
+   Mesh_Triangle_Node_t *head;
+   Mesh_Triangle_Node_t *tail;
+} Mesh_Triangle_List_t;
+
+Mesh_Triangle_List_t* Mesh_Triangle_List_create();
+
+void Mesh_Triangle_List_insert (Triangle, Mesh_Triangle_List_t);
+
+void Mesh_Triangle_List_union (Mesh_Triangle_List_t, Mesh_Triangle_List_t);
+
+Mesh_Triangle_Node_t* Mesh_Triangle_List_remove (Mesh_Triangle_List_t , Mesh_Triangle_Node_t);
